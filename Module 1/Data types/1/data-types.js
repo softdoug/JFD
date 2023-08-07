198755833aefc57a9fd9c383ca8985b65ca4cd9e
Softdoug <lev.vasilenko@gmail.com>
2023-08-07 13:01:42 +0300

    3rd lesson complete

---
+++ Module 1/Data types/1/data-types.js
@@ -0,0 +1,25 @@
+// String
+const string = 'text'
+console.log('string', string, typeof string)
+// Number
+const number = 1
+console.log('number', number, typeof number)
+// Boolean
+const boolean = true
+console.log('boolean', boolean, typeof boolean)
+// Null
+const nullName = null
+console.log('nullName', nullName, typeof nullName)
+// Undefined
+const undefinedName = undefined
+console.log('undefinedName', undefinedName, typeof undefinedName)
+// Object
+const obj = {one: 1, two: 2}
+console.log('obj', obj, typeof obj)
+// Symbol
+const symb = Symbol('example')
+console.log('symb', symb, typeof symb)
+// BigInt
+const bigInt = 12345678n
+console.log('bigInt', bigInt, typeof bigInt)
+
---
+++ Module 1/Data types/2/index.js
@@ -0,0 +1,13 @@
+// number
+let num = 1
+num = 'number is string'
+alert(num)
+// string
+let str = 'text'
+str = 123
+alert(str)
+// boolean
+let bool = true
+bool = null
+alert(bool)
+
