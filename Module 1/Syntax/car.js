fe253c1328c8f17d877a9bcc3d32b81c4aa22cd9
Softdoug <lev.vasilenko@gmail.com>
2023-08-07 12:06:26 +0300

    Lesson 2 is complete

---
+++ Module 1/Syntax/car.js
@@ -0,0 +1,21 @@
+const carName = 'Cadillac'
+let maxCarSpeed = '140 mph'
+let carOwner = 'John'
+
+alert(carName)
+alert(maxCarSpeed)
+alert(carOwner)
+
+maxCarSpeed = '135 mph'
+console.log('У машины изменилась скорость', maxCarSpeed)
+maxCarSpeed = '130 mph'
+console.log('У машины изменилась скорость', maxCarSpeed)
+maxCarSpeed = '125 mph'
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+carOwner = 'Steve'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Carol'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'mr. CarOwner'
+console.log('У машины изменился владелец', carOwner)
---
+++ Module 1/Syntax/index.js
@@ -0,0 +1,16 @@
+// JavaScript-код с созданием переменных
+
+/* Автор:
+Lev Vasilenko */
+
+const myName = 'Lev'
+const mySurname = 'Vasilenko'
+const myFavoriteDrink = 'Coffe'
+const myFavoriteAnimal = 'My cat'
+const myFavoriteProgrammingLanguage = 'JavaScript'
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
